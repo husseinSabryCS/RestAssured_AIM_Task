3e0c75bdc817799a85410f25c44b3e4a82afd2cd
Hussein Sabry <145670608+husseinSabryCS@users.noreply.github.com>
2025-07-16 16:03:37 +0300

    Update Basics.java

--- src/Basics.java
+++ src/Basics.java
@@ -62,61 +62,8 @@ String updateResponse = given().log().all().queryParam("key", "qaclick123").quer
 		
 		
 		
-		
-//		RestAssured.baseURI= "https://rahulshettyacademy.com";
-//		
-//		String response=given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
-//		.body(payload.AddPlace()).when().post("maps/api/place/add/json")
-//		.then().assertThat().statusCode(200).body("scope", equalTo("APP"))
-//		.header("server", "Apache/2.4.18 ").extract().response().asString();
-//		
-//		System.out.println(response);
-//		JsonPath js=new JsonPath(response); //for parsing Json
-//		String placeId=js.getString("place_id");
-//		
-//		System.out.println(placeId);
-//		
-//		//Update Place
-//		String newAddress = "Summer Walk, Africa";
-//		
-//		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
-//		.body("{\r\n" + 
-//				"\"place_id\":\""+placeId+"\",\r\n" + 
-//				"\"address\":\""+newAddress+"\",\r\n" + 
-//				"\"key\":\"qaclick123\"\r\n" + 
-//				"}").
-//		when().put("maps/api/place/update/json")
-//		.then().assertThat().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
-//		
-//		//Get Place
-//		
-//	String getPlaceResponse=	given().log().all().queryParam("key", "qaclick123")
-//		.queryParam("place_id",placeId)
-//		.when().get("maps/api/place/get/json")
-//		.then().assertThat().log().all().statusCode(200).extract().response().asString();
-//	JsonPath js1=ReUsableMethods.rawToJson(getPlaceResponse);
-//	String actualAddress =js1.getString("address");
-//	System.out.println(actualAddress);
-//	Assert.assertEquals(actualAddress, "Pacific ocean");
-	//Cucumber Junit, Testng
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
-		
-		
-		
-		
-		
-		
-		
-		
+	
 		
 		
 		
